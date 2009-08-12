c4d97f576e98da3e0c74e6cf57ac9b0507b0e115
Chris Wanstrath <chris@ozmm.org>
2009-08-11 18:06:36 -0700

    namespace queues

--- lib/resque.rb
+++ lib/resque.rb
@@ -18,22 +18,22 @@ class Resque
   #
 
   def push(queue, item)
-    @redis.rpush(queue, encode(item))
+    @redis.rpush(key(queue), encode(item))
   end
 
   def pop(queue)
-    decode @redis.lpop(queue)
+    decode @redis.lpop(key(queue))
   end
 
   def size(queue)
-    @redis.llen(queue)
+    @redis.llen(key(queue))
   end
 
   def peek(queue, start = 0, count = 1)
     if count == 1
-      decode @redis.lindex(queue, start)
+      decode @redis.lindex(key(queue), start)
     else
-      Array(@redis.lrange(queue, start, start+count-1)).map do |item|
+      Array(@redis.lrange(key(queue), start, start+count-1)).map do |item|
         decode item
       end
     end
@@ -64,5 +64,13 @@ class Resque
 
   def decode(object)
     Yajl::Parser.parse(object) if object
+  end
+
+  #
+  # namespacing
+  #
+
+  def key(queue)
+    "resque:#{queue}"
   end
 end
