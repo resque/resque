485490cc479d3288408a7ccee0e50b20a8647b80
Chris Wanstrath <chris@ozmm.org>
2009-08-28 22:20:52 -0700

    bring back the init.rb with hoptoad support

--- init.rb
+++ init.rb
@@ -4,6 +4,14 @@ if $redis
   Resque.redis = $redis
 elsif defined? RAILS_ROOT
   require 'yaml'
-  config = YAML.load_file File.join(RAILS_ROOT, 'config', 'resque.yml')
+  yaml = YAML.load_file File.join(RAILS_ROOT, 'config', 'resque.yml')
   Resque.redis = config[RAILS_ENV]
+
+  if toad = config['hoptoad']
+    Resque::Failure::Hoptoad.configure do |config|
+      config.api_key = toad['api_key']
+      config.secure = toad['secure']
+      config.subdomain = toad['subdomain']
+    end
+  end
 end
