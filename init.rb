f18f7fdcd5980732d9a678da05560373a4aa22c7
Chris Wanstrath <chris@ozmm.org>
2009-08-28 22:08:42 -0700

    No more init.rb or resque.yml. Do it yourself!

--- init.rb
+++
@@ -1,9 +0,0 @@
-require 'resque'
-
-if $redis
-  Resque.redis = $redis
-elsif defined? RAILS_ROOT
-  require 'yaml'
-  config = YAML.load_file File.join(RAILS_ROOT, 'config', 'resque.yml')
-  Resque.redis = config[RAILS_ENV]
-end
