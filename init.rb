fa7713cd1ee221ca29332e319dec166ac929571a
Chris Wanstrath <chris@ozmm.org>
2009-08-31 17:35:01 -0700

    make init.rb work with non-rails setups

--- init.rb
+++ init.rb
@@ -1,11 +1,31 @@
 require 'resque'
 
+# railsy
 root = ENV['RAILS_ROOT'] || (defined?(RAILS_ROOT) && RAILS_ROOT)
 env  = ENV['RAILS_ENV']  || (defined?(RAILS_ENV) && RAILS_ENV)
 
+# non-rails
+root ||= File.expand_path(ENV['CONFIG']) if ENV['CONFIG']
+env  ||= ENV['ENV']
+
+if !env
+  puts "** No ENV or RAILS_ENV found; assuming `development`"
+  env = 'development'
+end
+
 if root
-  require 'yaml'
-  config = YAML.load_file File.join(root, 'config', 'resque.yml')
+  yml  = 'resque.yml'
+  file = [ File.join(root, 'config', yml), File.join(root, yml) ].detect do |f|
+    File.exists? f
+  end
+
+  if file
+    require 'yaml'
+    config = YAML.file_load(file)
+  else
+    raise "Can't find resque.yml in #{root}"
+  end
+
   Resque.redis = config[env]
 
   if toad = config['hoptoad']
