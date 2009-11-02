160cc1244b3552a825e311bc49097fec9c2e8834
Chris Wanstrath <chris@ozmm.org>
2009-11-02 11:58:11 -0800

    stats docs

--- lib/resque/stat.rb
+++ lib/resque/stat.rb
@@ -1,29 +1,43 @@
 module Resque
+  # The stat subsystem. Used to keep track of integer counts.
+  #
+  # Get a stat:  Stat[name]
+  # Incr a stat: Stat.incr(name)
+  # Decr a stat: Stat.decr(name)
+  # Kill a stat: Stat.clear(name)
   module Stat
     extend self
+    extend Helpers
 
+    # Returns the int value of a stat, given a string stat name.
     def get(stat)
       redis.get("stat:#{stat}").to_i
     end
 
+    # Alias of `get`
     def [](stat)
       get(stat)
     end
 
+    # For a string stat name, increments the stat by one.
+    #
+    # Can optionally accept a second int parameter. The stat is then
+    # incremented by that amount.
     def incr(stat, by = 1)
       redis.incr("stat:#{stat}", by)
     end
 
+    # For a string stat name, decrements the stat by one.
+    #
+    # Can optionally accept a second int parameter. The stat is then
+    # decremented by that amount.
     def decr(stat, by = 1)
       redis.decr("stat:#{stat}", by)
     end
 
+    # Removes a stat from Redis, effectively setting it to 0.
     def clear(stat)
       redis.del("stat:#{stat}")
-    end
-
-    def redis
-      Resque.redis
     end
   end
 end
