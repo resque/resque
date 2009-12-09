6e3c88a58c8db1b8a2fb14a8b712f464077fc98a
Michael Dwan <mpdwan@gmail.com>
2009-12-09 15:35:34 -0600

    Converting JSON(..) => object.to_json/JSON.parse(..) in order to work with the JSON gem

--- lib/resque/helpers.rb
+++ lib/resque/helpers.rb
@@ -12,7 +12,7 @@ module Resque
       if defined? Yajl
         Yajl::Encoder.encode(object)
       else
-        JSON(object)
+        object.to_json
       end
     end
 
@@ -23,7 +23,7 @@ module Resque
       if defined? Yajl
         Yajl::Parser.parse(object, :check_utf8 => false)
       else
-        JSON(object)
+        JSON.parse(object)
       end
     end
 
