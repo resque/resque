ee4b70011d040c0e79cc75b7a8a3ff855a9897a9
Ryan Carver <ryan@fivesevensix.com>
2010-03-23 14:04:58 -0700

    add the job hooks api

--- lib/resque/job.rb
+++ lib/resque/job.rb
@@ -15,6 +15,10 @@ module Resque
     include Helpers
     extend Helpers
 
+    # Raise Resque::Job::DontPerform from a before_perform hook to
+    # abort the job.
+    DontPerform = Class.new(StandardError)
+
     # The worker object which is currently processing this job.
     attr_accessor :worker
 
@@ -101,7 +105,47 @@ module Resque
     # Calls #perform on the class given in the payload with the
     # arguments given in the payload.
     def perform
-      args ? payload_class.perform(*args) : payload_class.perform
+      job_args = args || []
+      job_was_performed = false
+
+      begin
+        # Execute before_perform hook. Abort the job gracefully if
+        # Resque::DontPerform is raised.
+        begin
+          if payload_class.respond_to?(:before_perform)
+            payload_class.before_perform(*job_args)
+          end
+        rescue DontPerform
+          return false
+        end
+
+        # Execute the job. Do it in an around_perform hook if available.
+        if payload_class.respond_to?(:around_perform)
+          payload_class.around_perform(*job_args) do
+            payload_class.perform(*job_args)
+            job_was_performed = true
+          end
+        else
+          payload_class.perform(*job_args)
+          job_was_performed = true
+        end
+
+        # Execute after_perform hook
+        if payload_class.respond_to?(:after_perform)
+          payload_class.after_perform(*job_args)
+        end
+
+        # Return true if the job was performed
+        return job_was_performed
+
+      # If an exception occurs during the job execution, look for an
+      # on_failure hook then re-raise.
+      rescue Object => e
+        if payload_class.respond_to?(:on_failure)
+          payload_class.on_failure(e, *job_args)
+        end
+        raise
+      end
     end
 
     # Returns the actual class constant represented in this job's payload.
