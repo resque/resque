1edd59bfc363b5a654cad4d35fc309936018e52f
Ryan Carver <ryan@fivesevensix.com>
2010-04-01 12:04:15 -0700

    simple linting to ensure your plugin names its hooks uniquely

--- lib/resque/plugin.rb
+++ lib/resque/plugin.rb
@@ -2,6 +2,20 @@ module Resque
   module Plugin
     extend self
 
+    LintError = Class.new(RuntimeError)
+
+    # Ensure that your plugin conforms to good hook naming conventions.
+    #
+    #   Resque::Plugin.lint(MyResquePlugin)
+    def lint(plugin)
+      (before_hooks(plugin) + around_hooks(plugin) + after_hooks(plugin)).each do |hook|
+        raise LintError, "#{plugin}.#{hook} is not namespaced" if hook =~ /perform$/
+      end
+      failure_hooks(plugin).each do |hook|
+        raise LintError, "#{plugin}.#{hook} is not namespaced" if hook =~ /failure$/
+      end
+    end
+
     def before_hooks(job)
       job.methods.grep(/^before_perform/).sort
     end
@@ -17,6 +31,5 @@ module Resque
     def failure_hooks(job)
       job.methods.grep(/^on_failure/).sort
     end
-
   end
 end
--- test/plugin_test.rb
+++ test/plugin_test.rb
@@ -35,3 +35,82 @@ context "Resque::Plugin finding hooks" do
   end
 end
 
+context "Resque::Plugin linting" do
+  module BadBefore
+    def self.before_perform; end
+  end
+  module BadAfter
+    def self.after_perform; end
+  end
+  module BadAround
+    def self.around_perform; end
+  end
+  module BadFailure
+    def self.on_failure; end
+  end
+
+  test "before_perform must be namespaced" do
+    begin
+      Resque::Plugin.lint(BadBefore)
+      assert false, "should have failed"
+    rescue Resque::Plugin::LintError => e
+      assert_equal "BadBefore.before_perform is not namespaced", e.message
+    end
+  end
+
+  test "after_perform must be namespaced" do
+    begin
+      Resque::Plugin.lint(BadAfter)
+      assert false, "should have failed"
+    rescue Resque::Plugin::LintError => e
+      assert_equal "BadAfter.after_perform is not namespaced", e.message
+    end
+  end
+
+  test "around_perform must be namespaced" do
+    begin
+      Resque::Plugin.lint(BadAround)
+      assert false, "should have failed"
+    rescue Resque::Plugin::LintError => e
+      assert_equal "BadAround.around_perform is not namespaced", e.message
+    end
+  end
+
+  test "on_failure must be namespaced" do
+    begin
+      Resque::Plugin.lint(BadFailure)
+      assert false, "should have failed"
+    rescue Resque::Plugin::LintError => e
+      assert_equal "BadFailure.on_failure is not namespaced", e.message
+    end
+  end
+
+  module GoodBefore
+    def self.before_perform1; end
+  end
+  module GoodAfter
+    def self.after_perform1; end
+  end
+  module GoodAround
+    def self.around_perform1; end
+  end
+  module GoodFailure
+    def self.on_failure1; end
+  end
+
+  test "before_perform1 is an ok name" do
+    Resque::Plugin.lint(GoodBefore)
+  end
+
+  test "after_perform1 is an ok name" do
+    Resque::Plugin.lint(GoodAfter)
+  end
+
+  test "around_perform1 is an ok name" do
+    Resque::Plugin.lint(GoodAround)
+  end
+
+  test "on_failure1 is an ok name" do
+    Resque::Plugin.lint(GoodFailure)
+  end
+end
