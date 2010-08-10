1840616f358a57bbceb6bd456c57d129af06bad1
Aaron Quint <aaron@quirkey.com>
2010-08-10 23:37:53 +0800

    Allow the Redis namespace to be set via "host:port/namespace"

--- lib/resque.rb
+++ lib/resque.rb
@@ -26,19 +26,24 @@ module Resque
   # Accepts:
   #   1. A 'hostname:port' string
   #   2. A 'hostname:port:db' string (to select the Redis db)
-  #   3. An instance of `Redis`, `Redis::Client`, `Redis::DistRedis`,
+  #   3. A 'hostname:port/namespace' string (to set the Redis namespace)
+  #   4. A redis URL string 'redis://host:port'
+  #   5. An instance of `Redis`, `Redis::Client`, `Redis::DistRedis`,
   #      or `Redis::Namespace`.
   def redis=(server)
     if server.respond_to? :split
+      namespace = :resque
+
       if server =~ /redis\:\/\//
         redis = Redis.connect(:url => server)
       else
+        server, namespace = server.split('/', 2)
         host, port, db = server.split(':')
         redis = Redis.new(:host => host, :port => port,
           :thread_safe => true, :db => db)
       end
 
-      @redis = Redis::Namespace.new(:resque, :redis => redis)
+      @redis = Redis::Namespace.new(namespace, :redis => redis)
     elsif server.respond_to? :namespace=
         @redis = server
     else
