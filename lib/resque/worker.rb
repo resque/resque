c50e8f5e3406de9b528fe87e347b9e2201d1393b
phiggins <pete@peterhiggins.org>
2010-11-02 17:21:58 -0700

    Simplify Resque::Worker.working, get rid of unneccessary mapped_mget.

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -30,12 +30,7 @@ module Resque
     # Returns an array of all worker objects currently processing
     # jobs.
     def self.working
-      names = all
-      return [] unless names.any?
-      names.map! { |name| "worker:#{name}" }
-      redis.mapped_mget(*names).keys.map do |key|
-        find key.sub("worker:", '')
-      end.compact
+      all.map {|name| find name.to_s }.compact
     end
 
     # Returns a single worker object. Accepts a string id.
