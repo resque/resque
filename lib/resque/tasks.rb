647d001a78c74036fb274fb0eb68dab12cae4292
vpuzzella <vincepuz@gmail.com>
2011-01-30 22:37:05 -0500

    Optionally write PID to a file

--- lib/resque/tasks.rb
+++ lib/resque/tasks.rb
@@ -15,6 +15,7 @@ namespace :resque do
       worker = Resque::Worker.new(*queues)
       worker.verbose = ENV['LOGGING'] || ENV['VERBOSE']
       worker.very_verbose = ENV['VVERBOSE']
+      `echo "#{Process.pid}" > #{ENV['PID']}` if ENV['PID']
     rescue Resque::NoQueueError
       abort "set QUEUE env var, e.g. $ QUEUE=critical,high rake resque:work"
     end
