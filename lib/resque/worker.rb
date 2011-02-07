c2cbcc169f763ee0367f069d80fede7ec7d15d67
Tom Brow <brow@punctual.(none)>
2011-02-07 19:27:37 +0000

    fix erroneous pruning of living workers

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -470,7 +470,7 @@ module Resque
     # Returns an array of string pids of all the other workers on this
     # machine. Useful when pruning dead workers on startup.
     def worker_pids
-      `ps -A -o pid,command | grep [r]esque | grep -i "resque-web"`.split("\n").map do |line|
+      `ps -A -o pid,command | grep [r]esque | grep -v "resque-web"`.split("\n").map do |line|
         line.split(' ')[0]
       end
     end
