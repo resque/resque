0181ee34e984348593e4281a596c333705036575
Andrea Campi <andrea.campi@zephirworks.com>
2011-02-10 00:29:12 +0100

    Pass /notice/server-environment/project-root.
    According to Hoptoad API documentation this is
    optional, but some third-party software using the
    same API is more strict and errors out if we don't
    provide it (Redmine, I'm looking at you).

--- lib/resque/failure/hoptoad.rb
+++ lib/resque/failure/hoptoad.rb
@@ -109,6 +109,7 @@ module Resque
           end
           x.tag!("server-environment") do
             x.tag!("environment-name",server_environment)
+            x.tag!("project-root", "RAILS_ROOT")
           end
 
         end
