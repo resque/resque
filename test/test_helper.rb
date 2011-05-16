7b282028bddd7fb81644b3427c2d83fa8fe23e54
Chris Wanstrath <chris@ozmm.org>
2011-05-16 13:28:14 -0700

    ensure we're using bundler's test env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,12 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup(:default, :test)
+Bundler.require(:default, :test)
+
 dir = File.dirname(File.expand_path(__FILE__))
 $LOAD_PATH.unshift dir + '/../lib'
 $TESTING = true
 require 'test/unit'
-require 'rubygems'
-require 'resque'
 
 begin
   require 'leftright'
