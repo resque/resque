d85097dc66efce1f58621adb0779bbd4989db54d
Chris Wanstrath <chris@ozmm.org>
2011-07-13 21:23:34 -0700

    OkJson won't work, raise if it's in use.

--- lib/resque/helpers.rb
+++ lib/resque/helpers.rb
@@ -1,5 +1,11 @@
 require 'multi_json'
 
+# OkJson won't work because it doesn't serialize symbols
+# in the same way yajl and json do.
+if MultiJson.engine.to_s == 'MultiJson::Engines::OkJson'
+  raise "Please install the yajl-ruby or json gem"
+end
+
 module Resque
   # Methods used by various classes in Resque.
   module Helpers
