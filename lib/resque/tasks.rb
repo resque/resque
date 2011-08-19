cb371b4b931ac425428676b645a8813ba2be485c
Chris Wanstrath <chris@ozmm.org>
2011-08-19 02:17:50 -0700

    Use RAILS_ROOT in preload task

--- lib/resque/tasks.rb
+++ lib/resque/tasks.rb
@@ -5,7 +5,7 @@ namespace :resque do
   task :setup
 
   desc "Start a Resque worker"
-  task :work => :setup do
+  task :work => [ :preload, :setup ] do
     require 'resque'
 
     queues = (ENV['QUEUES'] || ENV['QUEUE']).to_s.split(',')
@@ -39,11 +39,13 @@ namespace :resque do
 
     threads.each { |thread| thread.join }
   end
-end
 
-# Preload app files
-task :environment do
-  Dir['app/**/*.rb'].each do |file|
-    require file
+  # Preload app files if this is Rails
+  task :preload do
+    if defined? RAILS_ROOT
+      Dir["#{RAILS_ROOT}/app/**/*.rb"].each do |file|
+        require file
+      end
+    end
   end
 end
