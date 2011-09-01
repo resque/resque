2113127614c93a759056d16fae56a58f1ca763a3
Ryan G <ersatzryan@gmail.com>
2011-09-01 11:20:27 -0400

    Adds Airbrake support while continuing backwards compatibility for Hoptoad. Adds tests for Airbrake.

--- test/airbrake_test.rb
+++ test/airbrake_test.rb
@@ -1,25 +1,26 @@
+
 require 'test_helper'
 
 begin
-  require 'hoptoad_notifier'
+  require 'airbrake'
 rescue LoadError
-  warn "Install hoptoad_notifier gem to run Hoptoad tests."
+  warn "Install airbrake gem to run Airbrake tests."
 end
 
-if defined? HoptoadNotifier
-  require 'resque/failure/hoptoad'
-  context "Hoptoad" do
+if defined? Airbrake
+  require 'resque/failure/airbrake'
+  context "Airbrake" do
     test "should be notified of an error" do
       exception = StandardError.new("BOOM")
       worker = Resque::Worker.new(:test)
       queue = "test"
       payload = {'class' => Object, 'args' => 66}
 
-      HoptoadNotifier.expects(:notify_or_ignore).with(
+      Airbrake.expects(:notify_or_ignore).with(
         exception,
         :parameters => {:payload_class => 'Object', :payload_args => '66'})
 
-      backend = Resque::Failure::Hoptoad.new(exception, worker, queue, payload)
+      backend = Resque::Failure::Airbrake.new(exception, worker, queue, payload)
       backend.save
     end
   end
