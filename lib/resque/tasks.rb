d812da700523600b8cd5c70ee45aa2ceda18e3c9
Matt Wilson <mhw@hypomodern.com>
2011-09-23 01:24:48 -0400

    Change how we load the Rails environment.
    I was seeing errors running `rake resque:work` that boiled down to problems loading engines. The only way I could determine to fix it was to run `rake environment resque:work` or specifying a dependency on :environment from resque:setup.

--- lib/resque/tasks.rb
+++ lib/resque/tasks.rb
@@ -50,8 +50,8 @@ namespace :resque do
   # Preload app files if this is Rails
   task :preload => :setup do
     if defined?(Rails) && Rails.respond_to?(:application)
-      # Rails 3
-      Rails.application.eager_load!
+      # Rails 3.x
+      Rake::Task[:environment].invoke
     elsif defined?(Rails::Initializer)
       # Rails 2.3
       $rails_rake_task = false
