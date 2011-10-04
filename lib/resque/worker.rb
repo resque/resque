387c1535cbf25e2e18916f7bdba4d5e0d5fc3454
Aaron Quint <aaron@quirkey.com>
2011-10-04 14:21:51 +0200

    Assign the job's worker before calling `before_fork`
    Useful for inspecting the jobs woker in the hook

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -128,6 +128,7 @@ module Resque
 
         if not paused? and job = reserve
           log "got: #{job.inspect}"
+          job.worker = self
           run_hook :before_fork, job
           working_on job
 
@@ -160,6 +161,7 @@ module Resque
     def process(job = nil, &block)
       return unless job ||= reserve
 
+      job.worker = self
       working_on job
       perform(job, &block)
     ensure
@@ -388,7 +390,6 @@ module Resque
     # Given a job, tells Redis we're working on it. Useful for seeing
     # what workers are doing and when.
     def working_on(job)
-      job.worker = self
       data = encode \
         :queue   => job.queue,
         :run_at  => Time.now.strftime("%Y/%m/%d %H:%M:%S %Z"),
