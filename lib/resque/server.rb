3080af3258ae3f14fb1a5be895a6b5b5ed6d0684
Chris Wanstrath <chris@ozmm.org>
2011-10-20 17:27:02 -0700

    Use Sinatra's public_folder if it exists
    Fixes #421. Fixes #420.

--- lib/resque/server.rb
+++ lib/resque/server.rb
@@ -13,7 +13,13 @@ module Resque
     dir = File.dirname(File.expand_path(__FILE__))
 
     set :views,  "#{dir}/server/views"
-    set :public, "#{dir}/server/public"
+
+    if respond_to? :public_folder
+      set :public_folder, "#{dir}/server/public"
+    else
+      set :public, "#{dir}/server/public"
+    end
+
     set :static, true
 
     helpers do
