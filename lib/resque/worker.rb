bea944483bf8e9061ccacbf01796eb20abe45bc7
Igor Kozlov <gray.fragments@gmail.com>
2011-11-25 12:45:10 +0000

    use Kernel.exit instead of Kernel.exit!

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -132,14 +132,19 @@ module Resque
           run_hook :before_fork, job
           working_on job
 
-          if @child = fork
+          thread = Thread.new do
+            fork or begin
+              procline "Processing #{job.queue} since #{Time.now.to_i}"
+              perform(job, &block)
+              exit unless @cant_fork
+            end
+          end
+
+          unless @cant_fork
+            @child = thread.value
             srand # Reseeding
             procline "Forked #{@child} at #{Time.now.to_i}"
             Process.wait(@child)
-          else
-            procline "Processing #{job.queue} since #{Time.now.to_i}"
-            perform(job, &block)
-            exit! unless @cant_fork
           end
 
           done_working
