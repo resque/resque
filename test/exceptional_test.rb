8bb740d6854fd2913120f702d47215da8e7f83bd
Russ Smith <russ@bashme.org>
2012-03-05 07:42:44 -0800

    Exceptional error logging.

---
+++ test/exceptional_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+begin
+  require 'exceptoional'
+rescue LoadError
+  warn "Install exceptional gem to run Exceptional tests."
+end
+
+if defined? Exceptional
+  require 'resque/failure/exceptional'
+  context "Airbrake" do
+    test "should be notified of an error" do
+      exception = StandardError.new("BOOM")
+      worker = Resque::Worker.new(:test)
+      queue = "test"
+      payload = {'class' => Object, 'args' => 66}
+
+      Exceptional.expects(:notify_or_ignore).with(
+        exception,
+        :parameters => {:payload_class => 'Object', :payload_args => '66'})
+
+      backend = Resque::Failure::Exceptional.new(exception, worker, queue, payload)
+      backend.save
+    end
+  end
+end
