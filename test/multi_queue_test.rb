70ae9d0ad955d657e81c9b7f617b31dc867a12ba
Terence Lee <hone02@gmail.com>
2012-04-14 02:23:28 +0200

    test that nonblocking pop on MultiQueue doesn't block

--- test/multi_queue_test.rb
+++ test/multi_queue_test.rb
@@ -31,6 +31,16 @@ describe "Resque::MulitQueue" do
     assert_equal [bar, job], queue.pop(true)
   end
 
+  it "nonblocking pop doesn't block" do
+    foo   = Resque::Queue.new 'foo', redis, coder
+    bar   = Resque::Queue.new 'bar', redis, coder
+    queue = Resque::MultiQueue.new([foo, bar], redis)
+
+    assert_raises ThreadError do
+      queue.pop(true)
+    end
+  end
+
   it "blocks forever on pop" do
     foo   = Resque::Queue.new 'foo', redis, coder
     bar   = Resque::Queue.new 'bar', redis, coder
