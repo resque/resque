0eb629f4c1b7e7a84d2d1420f1c5f31a8ab21707
Alex Koppel <alex@alexkoppel.com>
2012-04-18 08:23:35 +0200

    Check for MultiJson::Adapters::OkJson

--- lib/resque/multi_json_coder.rb
+++ lib/resque/multi_json_coder.rb
@@ -4,7 +4,7 @@ require 'resque/coder'
 # OkJson won't work because it doesn't serialize symbols
 # in the same way yajl and json do.
 engine = MultiJson.respond_to?(:adapter) ? MultiJson.adapter : MultiJson.engine
-if engine.to_s == 'MultiJson::Engines::OkJson'
+if engine.to_s == 'MultiJson::Engines::OkJson' || engine.to_s == 'MultiJson::Adapters::OkJson'
   raise "Please install the yajl-ruby or json gem"
 end
 
