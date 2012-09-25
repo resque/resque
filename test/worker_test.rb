b450d5e617add07246da0d7d518f9429f17f4e3c
Tony Arcieri <tony.arcieri@gmail.com>
2012-09-25 13:22:51 -0600

    Only call after_fork if will_fork? is true
    This change provides consistent forking semantics across JRuby running
    in both 1.8 and 1.9 mode and resolves the current test failure.
    
    Conflicts:
    
    	test/worker_test.rb

--- test/worker_test.rb
+++ test/worker_test.rb
@@ -483,6 +483,7 @@ describe "Resque::Worker" do
   end
 
   it "Will call an after_fork hook after forking" do
+    Resque.redis.flushall
     $AFTER_FORK_CALLED = false
     Resque.after_fork = Proc.new { $AFTER_FORK_CALLED = true }
     workerA = Resque::Worker.new(:jobs)
@@ -490,7 +491,7 @@ describe "Resque::Worker" do
     assert !$AFTER_FORK_CALLED
     Resque::Job.create(:jobs, SomeJob, 20, '/tmp')
     workerA.work(0)
-    assert $AFTER_FORK_CALLED
+    assert $AFTER_FORK_CALLED == workerA.will_fork?
   end
 
   it "Will not call an after_fork hook when the worker can't fork" do
