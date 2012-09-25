2c2f1d0c5ecb0eeb260649444ac5002e3759f8e7
Pan Thomakos <pan.thomakos@gmail.com>
2012-09-25 11:06:16 -0700

    Added missing tests and pause attr_writers.

--- test/resque_hook_test.rb
+++ test/resque_hook_test.rb
@@ -25,7 +25,7 @@ describe "Resque Hooks" do
     assert_equal [], Resque.after_fork
   end
 
-  it 'it calls before_first_fork once' do
+  it 'calls before_first_fork once' do
     counter = 0
 
     Resque.before_first_fork { counter += 1 }
@@ -36,7 +36,7 @@ describe "Resque Hooks" do
     assert_equal(1, counter)
   end
 
-  it 'it calls before_fork before each job' do
+  it 'calls before_fork before each job' do
     counter = 0
 
     Resque.before_fork { counter += 1 }
@@ -47,7 +47,7 @@ describe "Resque Hooks" do
     assert_equal(2, counter)
   end
 
-  it 'it calls after_fork after each job' do
+  it 'calls after_fork after each job' do
     counter = 0
 
     Resque.after_fork { counter += 1 }
@@ -58,28 +58,28 @@ describe "Resque Hooks" do
     assert_equal(2, counter)
   end
 
-  it 'it calls before_first_fork before forking' do
+  it 'calls before_first_fork before forking' do
     Resque.before_first_fork { assert(!$called) }
 
     Resque::Job.create(:jobs, CallNotifyJob)
     @worker.work(0)
   end
 
-  it 'it calls before_fork before forking' do
+  it 'calls before_fork before forking' do
     Resque.before_fork { assert(!$called) }
 
     Resque::Job.create(:jobs, CallNotifyJob)
     @worker.work(0)
   end
 
-  it 'it calls after_fork after forking' do
+  it 'calls after_fork after forking' do
     Resque.after_fork { assert($called) }
 
     Resque::Job.create(:jobs, CallNotifyJob)
     @worker.work(0)
   end
 
-  it 'it registeres multiple before_first_forks' do
+  it 'registeres multiple before_first_forks' do
     first = false
     second = false
 
@@ -92,7 +92,7 @@ describe "Resque Hooks" do
     assert(first && second)
   end
 
-  it 'it registers multiple before_forks' do
+  it 'registers multiple before_forks' do
     first = false
     second = false
 
@@ -105,7 +105,7 @@ describe "Resque Hooks" do
     assert(first && second)
   end
 
-  it 'it registers multiple after_forks' do
+  it 'registers multiple after_forks' do
     first = false
     second = false
 
@@ -115,6 +115,42 @@ describe "Resque Hooks" do
 
     assert(!first && !second)
     @worker.work(0)
+    assert(first && second)
+  end
+
+  it 'registers multiple before_pause hooks' do
+    first = false
+    second = false
+
+    Resque.before_pause { first = true }
+    Resque.before_pause { second = true }
+
+    @worker.pause_processing
+
+    assert(!first && !second)
+
+    t = Thread.start { sleep(0.1); Process.kill('CONT', @worker.pid) }
+    @worker.work(0)
+    t.join
+
+    assert(first && second)
+  end
+
+  it 'registers multiple after_pause hooks' do
+    first = false
+    second = false
+
+    Resque.after_pause { first = true }
+    Resque.after_pause { second = true }
+
+    @worker.pause_processing
+
+    assert(!first && !second)
+
+    t = Thread.start { sleep(0.1); Process.kill('CONT', @worker.pid) }
+    @worker.work(0)
+    t.join
+
     assert(first && second)
   end
 end
