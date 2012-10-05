d3ae69f286712ef363097c4bddc251b3f29a96e1
Terence Lee <hone02@gmail.com>
2012-10-05 00:08:16 -0500

    remove module_function on logger module

--- lib/resque/logging.rb
+++ lib/resque/logging.rb
@@ -1,7 +1,6 @@
 module Resque
   # Include this module in classes you wish to have logging facilities
   module Logging
-    module_function
 
     # Thunk to the logger's own log method (if configured)
     def self.log(severity, message)
