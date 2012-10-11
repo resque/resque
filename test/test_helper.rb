7d852625ec7323a511537279143d9d5da70d4e6e
Terence Lee <hone02@gmail.com>
2012-10-11 17:36:01 -0500

    using the minitest gem, don't need to redefine it

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -138,22 +138,6 @@ class Time
   self.fake_time = nil
 end
 
-# From minitest/unit
-def capture_io
-  require 'stringio'
-
-  orig_stdout, orig_stderr         = $stdout, $stderr
-  captured_stdout, captured_stderr = StringIO.new, StringIO.new
-  $stdout, $stderr                 = captured_stdout, captured_stderr
-
-  yield
-
-  return captured_stdout.string, captured_stderr.string
-ensure
-  $stdout = orig_stdout
-  $stderr = orig_stderr
-end
-
 # Log to log/test.log
 def reset_logger
   $test_logger ||= Logger.new(File.open(File.expand_path("../../log/test.log", __FILE__), "w"))
