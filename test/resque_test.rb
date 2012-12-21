4ec13a3bdd58199d154924c99cd0165002e524cf
Terence Lee <hone02@gmail.com>
2012-12-21 18:14:18 -0500

    sort test results for arrays

--- test/resque_test.rb
+++ test/resque_test.rb
@@ -199,7 +199,7 @@ context "Resque" do
   test "knows what queues it is managing" do
     assert_equal %w( people ), Resque.queues
     Resque.push(:cars, { 'make' => 'bmw' })
-    assert_equal %w( cars people ), Resque.queues
+    assert_equal %w( cars people ).sort, Resque.queues.sort
   end
 
   test "queues are always a list" do
@@ -209,7 +209,7 @@ context "Resque" do
 
   test "can delete a queue" do
     Resque.push(:cars, { 'make' => 'bmw' })
-    assert_equal %w( cars people ), Resque.queues
+    assert_equal %w( cars people ).sort, Resque.queues.sort
     Resque.remove_queue(:people)
     assert_equal %w( cars ), Resque.queues
     assert_equal nil, Resque.pop(:people)
