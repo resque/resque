abacb57b01b96467202248ced1bb022cb7af8efc
sanemat <o.gata.ken@gmail.com>
2013-01-01 10:14:09 +0900

    Split invalid characters test

---
+++ test/resque_invalid_characters_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+describe "Resque" do
+  before do
+    Resque.redis.flushall
+
+    Resque.push(:people, { 'name' => 'chris' })
+    Resque.push(:people, { 'name' => 'bob' })
+    Resque.push(:people, { 'name' => 'mark' })
+    @original_redis = Resque.redis
+  end
+
+  after do
+    Resque.redis = @original_redis
+  end
+
+  if defined?(RUBY_ENGINE) && RUBY_ENGINE != "rbx"
+    # See https://github.com/defunkt/resque/issues/769
+    it "rescues jobs with invalid UTF-8 characters" do
+      Resque.logger = DummyLogger.new
+      begin
+        Resque.enqueue(SomeMethodJob, "Invalid UTF-8 character \xFF")
+        messages = Resque.logger.messages
+      rescue Exception => e
+        assert false, e.message
+      ensure
+        reset_logger
+      end
+      message = "Invalid UTF-8 character in job: \"\\xFF\" from ASCII-8BIT to UTF-8"
+      assert_includes messages, message
+    end
+  end
+end
--- test/resque_test.rb
+++ test/resque_test.rb
@@ -157,23 +157,6 @@ describe "Resque" do
       Resque.validate(SomeJob)
     end
   end
-  
-  if defined?(RUBY_ENGINE) && RUBY_ENGINE != "rbx"
-    # See https://github.com/defunkt/resque/issues/769
-    it "rescues jobs with invalid UTF-8 characters" do
-      Resque.logger = DummyLogger.new
-      begin
-        Resque.enqueue(SomeMethodJob, "Invalid UTF-8 character \xFF")
-        messages = Resque.logger.messages
-      rescue Exception => e
-        assert false, e.message
-      ensure
-        reset_logger
-      end
-      message = "Invalid UTF-8 character in job: \"\\xFF\" from ASCII-8BIT to UTF-8"
-      assert_includes messages, message
-    end
-  end
 
   it "can put items on a queue" do
     assert Resque.push(:people, { 'name' => 'jon' })
