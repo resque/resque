48e842212e5e9cffc39dc628dc16a8cef09393e3
Philipp Fehre <philipp.fehre@googlemail.com>
2013-03-07 09:51:24 +0100

    Added install generator to ease the setup
    When you setup resque you have to do the same stuff as documented in the
    readme, mostly line by line, so I moved it in a generator to be called
    via resque:install.

---
+++ lib/generators/resque/install_generator.rb
@@ -0,0 +1,13 @@
+require "rails"
+
+module Resque::Generators
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+
+    def copy_files
+      copy_file "resque.rake", "lib/tasks/resque.rake"
+      copy_file "resque.rb", "config/initializers/resque.rb"
+      copy_file "resque.yml", "config/resque.yml"
+    end
+  end
+end
