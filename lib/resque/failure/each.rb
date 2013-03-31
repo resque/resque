b1fb7891b093c6031062f9bf1512f56465ee359b
Tony Miller <mcfiredrill@gmail.com>
2013-03-31 11:50:20 -0700

    In .each, make sure that the proper array is created if there is only one failure.
    This fixes resque/resque-web#6

--- lib/resque/failure/each.rb
+++ lib/resque/failure/each.rb
@@ -2,7 +2,7 @@ module Resque
   module Failure
     module Each
       def each(offset = 0, limit = self.count, queue = :failed, class_name = nil)
-        items = Array(all(offset, limit, queue))
+        items = [all(offset, limit, queue)].flatten
         items.each_with_index do |item, i|
           if !class_name || (item['payload'] && item['payload']['class'] == class_name)
             yield offset + i, item
