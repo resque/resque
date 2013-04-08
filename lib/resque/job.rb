93eca9a7b0b9a899d37347a0dcd6c4ca9039fc6d
Juan Alvarez <alce@mac.com>
2013-04-08 14:57:11 -0700

    Remove Helpers from Job class.
    All of these methods will obviously need to be cleaned up
    later, right now we're just killing Helpers

--- lib/resque/job.rb
+++ lib/resque/job.rb
@@ -14,8 +14,56 @@ module Resque
   #   klass = Resque::Job.constantize(job.payload['class'])
   #   klass.perform(*job.payload['args'])
   class Job
-    include Helpers
-    extend Helpers
+    def redis
+      Resque.redis
+    end
+
+    def self.redis
+      Resque.redis
+    end
+
+    def self.encode(object)
+      Resque.coder.encode(object)
+    end
+
+    def self.decode(object)
+      Resque.coder.decode(object)
+    end
+
+    def encode(object)
+      Resque.coder.encode(object)
+    end
+
+    def decode(object)
+      Resque.coder.decode(object)
+    end
+
+    def constantize(camel_cased_word)
+      names = camel_cased_word.to_s.split('::')
+      names.shift if names.empty? || names.first.empty?
+
+      names.inject(Object) do |constant, name|
+        if constant == Object
+          constant.const_get(name)
+        else
+          candidate = constant.const_get(name)
+          args = Module.method(:const_defined?).arity != 1 ? [false] : []
+          next candidate if constant.const_defined?(name, *args)
+          next candidate unless Object.const_defined?(name)
+
+          # Go down the ancestors to check it it's owned
+          # directly before we reach Object or the end of ancestors.
+          constant = constant.ancestors.inject do |const, ancestor|
+            break const    if ancestor == Object
+            break ancestor if ancestor.const_defined?(name, *args)
+            const
+          end
+
+          # owner is in Object, so raise
+          constant.const_get(name, false)
+        end
+      end
+    end
 
     # Raise Resque::Job::DontPerform from a before_perform hook to
     # abort the job.
