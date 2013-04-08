054f308a22754af49812cbde975e508826207603
Juan Alvarez <alce@mac.com>
2013-04-08 15:11:54 -0700

    Removed helpers from Worker.
    Why is constantize needed on the worker class? Seems bad.

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -10,9 +10,60 @@ module Resque
   # It also ensures workers are always listening to signals from you,
   # their master, and can react accordingly.
   class Worker
-    extend  Resque::Helpers
-    include Resque::Helpers
     include Resque::Logging
+
+    def redis
+      Resque.redis
+    end
+
+    def self.redis
+      Resque.redis
+    end
+    
+    # Tries to find a constant with the name specified in the argument string.
+    #
+    #   'Module'.constantize     # => Module
+    #   'Test::Unit'.constantize # => Test::Unit
+    #
+    # The name is assumed to be the one of a top-level constant, no matter
+    # whether it starts with "::" or not. No lexical context is taken into
+    # account:
+    #
+    #   C = 'outside'
+    #   module M
+    #     C = 'inside'
+    #     C               # => 'inside'
+    #     'C'.constantize # => 'outside', same as ::C
+    #   end
+    #
+    # NameError is raised when the name is not in CamelCase or the constant is
+    # unknown.
+    def self.constantize(camel_cased_word)
+      names = camel_cased_word.to_s.split('::')
+      names.shift if names.empty? || names.first.empty?
+
+      names.inject(Object) do |constant, name|
+        if constant == Object
+          constant.const_get(name)
+        else
+          candidate = constant.const_get(name)
+          args = Module.method(:const_defined?).arity != 1 ? [false] : []
+          next candidate if constant.const_defined?(name, *args)
+          next candidate unless Object.const_defined?(name)
+
+          # Go down the ancestors to check it it's owned
+          # directly before we reach Object or the end of ancestors.
+          constant = constant.ancestors.inject do |const, ancestor|
+            break const    if ancestor == Object
+            break ancestor if ancestor.const_defined?(name, *args)
+            const
+          end
+
+          # owner is in Object, so raise
+          constant.const_get(name, false)
+        end
+      end
+    end
 
     # Boolean indicating whether this worker can or can not fork.
     # Automatically set if a fork(2) fails.
