6655d1b88719aa2104a979daf2b9802bf8c5d8e6
Steve Klabnik <steve@steveklabnik.com>
2013-04-10 23:04:47 -0700

    We don't need this extra #constantize.
    This was added for some reason when we were removing Helpers. We don't
    use this one, we use the monkeypatch on String.

--- lib/resque/job.rb
+++ lib/resque/job.rb
@@ -17,33 +17,6 @@ module Resque
   #   klass = job.payload['class'].to_s.constantize
   #   klass.perform(*job.payload['args'])
   class Job
-    def constantize(camel_cased_word)
-      names = camel_cased_word.to_s.split('::')
-      names.shift if names.empty? || names.first.empty?
-
-      names.inject(Object) do |constant, name|
-        if constant == Object
-          constant.const_get(name)
-        else
-          candidate = constant.const_get(name)
-          args = Module.method(:const_defined?).arity != 1 ? [false] : []
-          next candidate if constant.const_defined?(name, *args)
-          next candidate unless Object.const_defined?(name)
-
-          # Go down the ancestors to check it it's owned
-          # directly before we reach Object or the end of ancestors.
-          constant = constant.ancestors.inject do |const, ancestor|
-            break const    if ancestor == Object
-            break ancestor if ancestor.const_defined?(name, *args)
-            const
-          end
-
-          # owner is in Object, so raise
-          constant.const_get(name, false)
-        end
-      end
-    end
-
     # The worker object which is currently processing this job.
     attr_accessor :worker
 
