97ec5ae70814cf8e1d14f93b14808ed9781a05e5
Kevin Shekleton <kevin.shekleton@gmail.com>
2013-05-02 13:31:25 -0500

    Add unit tests for Resque::Coder

---
+++ test/resque/coder_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+describe Resque::Coder do
+  before do
+    @coder = Resque::Coder.new
+  end
+
+  describe '#encode' do
+    it 'should raise an exception' do
+      assert_raises(Resque::EncodeException) {@coder.encode(nil)}
+    end
+
+    it 'aliased #dump should raise an exception' do
+      assert_raises(Resque::EncodeException) {@coder.dump(nil)}
+    end
+  end
+
+  describe '#decode' do
+    it 'should raise an exception' do
+      assert_raises(Resque::DecodeException) {@coder.decode(nil)}
+    end
+
+    it 'aliased #load should raise an exception' do
+      assert_raises(Resque::DecodeException) {@coder.load(nil)}
+    end
+  end
+end
