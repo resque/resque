resque/resque

commit 890ca9f2de24562467d811cbc14e0b62aac2c6ae

A tiny refactoring.
I generally don't like the 'extract into a module' refactoring, but this
code was duplicated between the two. I only noticed because of Code
Climate, and it turns out that semantically, there was some code which
was technically different. It's the `Array()` part of this request.

I glanced at a few other methods, and it seems like there's a lot of
duplication in general. I bet this module could actually turn into some
kind of superclass at some moment in the future, but right now, I'm just
trying to get a handle on bits of things.

lib/resque/failure/each.rb
@@ -1,0 +1,14 @@
+module Resque
+  module Failure
+    module Each
+      def each(offset = 0, limit = self.count, queue = :failed, class_name = nil)
+        items = Array(all(offset, limit, queue))
+        items.each_with_index do |item, i|
+          if !class_name || (item['payload'] && item['payload']['class'] == class_name)
+            yield offset + i, item
+          end
+        end
+      end
+    end
+  end
+end

lib/resque/failure/redis.rb
@@ -1,4 +1,5 @@
 require 'time'
+require 'resque/failure/each'
 
 module Resque
   module Failure
@@ -40,13 +41,7 @@ module Resque
         Resque.list_range(:failed, offset, limit)
       end
 
-      def self.each(offset = 0, limit = self.count, queue = :failed, class_name = nil)
-        Array(all(offset, limit, queue)).each_with_index do |item, i|
-          if !class_name || (item['payload'] && item['payload']['class'] == class_name)
-            yield offset + i, item
-          end
-        end
-      end
+      include Each
 
       def self.clear(queue = nil)
         raise ArgumentError, "invalid queue: #{queue}" if queue && queue.to_s == "failed"

lib/resque/failure/redis_multi_queue.rb
@@ -1,3 +1,5 @@
+require 'resque/failure/each'
+
 module Resque
   module Failure
     # A Failure backend that stores exceptions in Redis. Very simple but
@@ -41,15 +43,7 @@ module Resque
         Array(Resque.redis.smembers(:failed_queues))
       end
 
-      def self.each(offset = 0, limit = self.count, queue = :failed, class_name = nil)
-        items = all(offset, limit, queue)
-        items = [items] unless items.is_a? Array
-        items.each_with_index do |item, i|
-          if !class_name || (item['payload'] && item['payload']['class'] == class_name)
-            yield offset + i, item
-          end
-        end
-      end
+      include Each
 
       def self.clear(queue = :failed)
         Resque.redis.del(queue)

commit 2be0ed4bd336fa1c611651353370bb09afa16620

Ensure that the Redis and RedisMultiQueue failure types correctly extend (not include) Resque::Failure::Each. This fixes a breaking change introduced in 890ca9f.
Additionally, the refactoring also changed the behavior of ::each such
that if there were a single failure, the given block with yield to each
attribute of the failure rather than just the single failure.

Fixes #992

lib/resque/failure/each.rb
@@ -2,7 +2,12 @@ module Resque
   module Failure
     module Each
       def each(offset = 0, limit = self.count, queue = :failed, class_name = nil)
-        items = Array(all(offset, limit, queue))
+        # Ensure items is always a list, which is especially important when
+        # just a single failure is returned. One would think #all should
+        # always be returning a list but at this time, it does not in the
+        # case of only a single failure.
+        # See https://github.com/resque/resque/pull/916 for more history/context
+        items = [all(offset, limit, queue)].flatten
         items.each_with_index do |item, i|
           if !class_name || (item['payload'] && item['payload']['class'] == class_name)
             yield offset + i, item

lib/resque/failure/redis.rb
@@ -41,7 +41,7 @@ module Resque
         Resque.list_range(:failed, offset, limit)
       end
 
-      include Each
+      extend Each
 
       def self.clear(queue = nil)
         check_queue(queue)

lib/resque/failure/redis_multi_queue.rb
@@ -43,7 +43,7 @@ module Resque
         Array(Resque.backend.store.smembers(:failed_queues))
       end
 
-      include Each
+      extend Each
 
       def self.clear(queue = :failed)
         Resque.backend.store.del(queue)

test/resque/failure/each_test.rb
@@ -1,0 +1,86 @@
+require 'test_helper'
+require 'resque/failure/redis'
+
+describe Resque::Failure::Each do
+  after do
+    Resque::Failure.clear
+  end
+
+  describe 'each' do
+    it 'should properly iterate over the default range for a single failure' do
+      save_failures('T1')
+
+      n = 0
+      Resque::Failure::Redis.each do |i, failure|
+        assert_equal 0, i
+        assert_equal 'T1', failure['payload']['class']
+        n = n + 1
+      end
+
+      assert_equal 1, n
+    end
+
+    it 'should properly iterate over the default range for multiple failures' do
+      expected_failure_classes = ['T1', 'T2']
+      save_failures('T1', 'T2')
+
+      n = 0
+      Resque::Failure::Redis.each do |i, failure|
+        assert_equal n, i
+        assert_equal expected_failure_classes[n], failure['payload']['class']
+        n = n + 1
+      end
+
+      assert_equal 2, n
+    end
+
+    it 'should properly iterate over the specified range for multiple failures' do
+      save_failures('T1', 'T2', 'T3')
+
+      n = 0
+      Resque::Failure::Redis.each(1, 1) do |i, failure|
+        assert_equal 1, i
+        assert_equal 'T2', failure['payload']['class']
+        n = n + 1
+      end
+
+      assert_equal 1, n
+    end
+
+    it 'should only find the desired classes of errors' do
+      save_failures('T1', 'T2')
+
+      n = 0
+      Resque::Failure::Redis.each(0, 2, :failed, 'T2') do |i, failure|
+        assert_equal 1, i
+        assert_equal 'T2', failure['payload']['class']
+        n = n + 1
+      end
+
+      assert_equal 1, n
+    end
+
+    it 'should only find the desired classes of errors within the specified range' do
+      save_failures('T1', 'T2', 'T2', 'T3')
+
+      n = 0
+      Resque::Failure::Redis.each(2, 4, :failed, 'T2') do |i, failure|
+        assert_equal 2, i
+        assert_equal 'T2', failure['payload']['class']
+        n = n + 1
+      end
+
+      assert_equal 1, n
+    end
+  end
+
+  private
+
+  def save_failures(*classes)
+    classes.each do |klass|
+      failure = Resque::Failure::Redis.new(Exception.new,
+                                           nil, :test, {'class' => klass})
+      failure.save
+    end
+  end
+end