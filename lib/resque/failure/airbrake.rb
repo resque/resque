df10e174708d7f9429467e1360667f05150cb4f8
Jake Wilkins <pablo_honey@me.com>
2013-05-17 18:51:37 +0000

    Remove hoptoad failure backend and removes configure for airbrake.
    Hoptoad no longer exists, so we don't need it.
    
    Now that we only have one backend, we pull the thoughtbot one up into
    airbrake.
    
    configure is gone because it just did global config. Instead of running
    the block, do this instead:
    
        Resque::Failure.backend = Resque::Failure::Airbrake

--- lib/resque/failure/airbrake.rb
+++ lib/resque/failure/airbrake.rb
@@ -4,14 +4,31 @@ rescue LoadError
   raise "Can't find 'airbrake' gem. Please add it to your Gemfile or install it."
 end
 
-require 'resque/failure/thoughtbot'
-
 module Resque
   module Failure
     class Airbrake < Base
-      include Resque::Failure::Thoughtbot
+      def self.count(queue = nil, class_name = nil)
+        # We can't get the total # of errors from Hoptoad so we fake it
+        # by asking Resque how many errors it has seen.
+        Stat[:failed]
+      end
 
-      @klass = ::Airbrake
+      def save
+        ::Airbrake.notify_or_ignore(exception,
+          :parameters => {
+            :payload_class => payload['class'].to_s,
+            :payload_args => payload['args'].inspect
+          },
+          :component => 'resque',
+          :action => action
+        )
+      end
+
+      def action
+        action = payload['class'].to_s
+        action = action.underscore if action.respond_to?(:underscore)
+        action
+      end
     end
   end
 end
