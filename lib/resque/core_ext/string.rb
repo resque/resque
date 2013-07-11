0f45432b60cfae5a39913155fa977689e6f1c992
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-07-11 10:04:04 +0000

    Apply same treatment to String core_exts as we did to Hash ones to get them to show up in YARD (they still don't, but at least we have consistency?); Fixes bug like #1036 with String, ensures that our constantize extension only gets added if it isn't present already. Best viewed with -w.

--- lib/resque/core_ext/string.rb
+++ lib/resque/core_ext/string.rb
@@ -1,50 +1,52 @@
 # encoding: utf-8
 # Core extensions on String
 class String
-  # Tries to find a constant with the name specified in the argument string.
-  #
-  #   'Module'.constantize     # => Module
-  #   'Test::Unit'.constantize # => Test::Unit
-  #
-  # The name is assumed to be the one of a top-level constant, no matter
-  # whether it starts with "::" or not. No lexical context is taken into
-  # account:
-  #
-  #   C = 'outside'
-  #   module M
-  #     C = 'inside'
-  #     C               # => 'inside'
-  #     'C'.constantize # => 'outside', same as ::C
-  #   end
-  #
-  # NameError is raised when the name is not in CamelCase or the constant is
-  # unknown.
-  # Defined IFF not already defined elsewhere (e.g., ActiveSupport)
-  # @return [Object] - typically returns a Class or Module
-  def constantize
-    names = self.split('::')
-    names.shift if names.empty? || names.first.empty?
+  unless method_defined?(:constantize)
+    # Tries to find a constant with the name specified in the argument string.
+    #
+    #   'Module'.constantize     # => Module
+    #   'Test::Unit'.constantize # => Test::Unit
+    #
+    # The name is assumed to be the one of a top-level constant, no matter
+    # whether it starts with "::" or not. No lexical context is taken into
+    # account:
+    #
+    #   C = 'outside'
+    #   module M
+    #     C = 'inside'
+    #     C               # => 'inside'
+    #     'C'.constantize # => 'outside', same as ::C
+    #   end
+    #
+    # NameError is raised when the name is not in CamelCase or the constant is
+    # unknown.
+    # Defined IFF not already defined elsewhere (e.g., ActiveSupport)
+    # @return [Object] - typically returns a Class or Module
+    def constantize
+      names = self.split('::')
+      names.shift if names.empty? || names.first.empty?
 
-    names.inject(Object) do |constant, name|
-      if constant == Object
-        constant.const_get(name)
-      else
-        candidate = constant.const_get(name)
-        args = Module.method(:const_defined?).arity != 1 ? [false] : []
-        next candidate if constant.const_defined?(name, *args)
-        next candidate unless Object.const_defined?(name)
+      names.inject(Object) do |constant, name|
+        if constant == Object
+          constant.const_get(name)
+        else
+          candidate = constant.const_get(name)
+          args = Module.method(:const_defined?).arity != 1 ? [false] : []
+          next candidate if constant.const_defined?(name, *args)
+          next candidate unless Object.const_defined?(name)
 
-        # Go down the ancestors to check it it's owned
-        # directly before we reach Object or the end of ancestors.
-        constant = constant.ancestors.inject do |const, ancestor|
-          break const    if ancestor == Object
-          break ancestor if ancestor.const_defined?(name, *args)
-          const
+          # Go down the ancestors to check it it's owned
+          # directly before we reach Object or the end of ancestors.
+          constant = constant.ancestors.inject do |const, ancestor|
+            break const    if ancestor == Object
+            break ancestor if ancestor.const_defined?(name, *args)
+            const
+          end
+
+          # owner is in Object, so raise
+          constant.const_get(name, false)
         end
-
-        # owner is in Object, so raise
-        constant.const_get(name, false)
       end
     end
-  end unless String.respond_to?(:constantize)
+  end
 end
