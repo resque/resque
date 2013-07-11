1e3c942c76c5ac7b85f7eff8fc9dc59cf3e83395
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-07-11 09:49:57 +0000

    yarddoc wasn't picking up Hash core_ext documentation because of the trailing unless; diff best viewed with -w.

--- lib/resque/core_ext/hash.rb
+++ lib/resque/core_ext/hash.rb
@@ -1,30 +1,36 @@
 # encoding: utf-8
 # Core extensions on Hash
 class Hash
-  # Returns a duplicate of self with symbolized keys
-  # Defined IFF not already defined elsewhere (e.g., ActiveSupport)
-  # @return [Hash<Symbol,Object>]
-  def symbolize_keys
-    inject({}) do |options, (key, value)|
-      options[(key.to_sym rescue key) || key] = value
-      options
+  unless method_defined?(:symbolize_keys)
+    # Returns a duplicate of self with symbolized keys
+    # Defined IFF not already defined elsewhere (e.g., ActiveSupport)
+    # @return [Hash<Symbol,Object>]
+    def symbolize_keys
+      inject({}) do |options, (key, value)|
+        options[(key.to_sym rescue key) || key] = value
+        options
+      end
     end
-  end unless method_defined?(:symbolize_keys)
+  end
 
-  # Destructively replaces keys in self with symbolized keys
-  # Defined IFF not already defined elsewhere (e.g., ActiveSupport)
-  # @return [Hash<Symbol,Object>]
-  def symbolize_keys!
-    self.replace(self.symbolize_keys)
-  end unless method_defined?(:symbolize_keys!)
+  unless method_defined?(:symbolize_keys!)
+    # Destructively replaces keys in self with symbolized keys
+    # Defined IFF not already defined elsewhere (e.g., ActiveSupport)
+    # @return [Hash<Symbol,Object>]
+    def symbolize_keys!
+      self.replace(self.symbolize_keys)
+    end
+  end
 
-  # Return a subset of self
-  # Defined IFF not already defined elsewhere (e.g., ActiveSupport)
-  # @param keys [Array<Object>]
-  # @return [Hash<Object,Object>]
-  def slice(*keys)
-    hash = self.class.new
-    keys.each { |k| hash[k] = self[k] if has_key?(k) }
-    hash
-  end unless method_defined?(:slice)
+  unless method_defined?(:slice)
+    # Return a subset of self
+    # Defined IFF not already defined elsewhere (e.g., ActiveSupport)
+    # @param keys [Array<Object>]
+    # @return [Hash<Object,Object>]
+    def slice(*keys)
+      hash = self.class.new
+      keys.each { |k| hash[k] = self[k] if has_key?(k) }
+      hash
+    end
+  end
 end
