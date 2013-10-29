d62f9f62396e39b823442b7a2158ddc6c59f2574
David Wilkie <dwilkie@gmail.com>
2013-10-29 17:00:24 +0700

    use let instead of instance variables in job_performer_test

--- test/resque/job_performer_test.rb
+++ test/resque/job_performer_test.rb
@@ -1,15 +1,13 @@
 require 'test_helper'
 
 describe Resque::JobPerformer do
-  before do
-    @mock          = MiniTest::Mock.new
-    @job_performer = Resque::JobPerformer.new
-    @job_args = [:foo]
-  end
+  let(:mock) { MiniTest::Mock.new }
+  let(:job_performer) { Resque::JobPerformer.new }
+  let(:job_args) { [:foo] }
 
   describe '#perform' do
-    before do
-      @options  = {
+    let(:options) {
+      {
         :before => [
           :before_one,
           :before_two
@@ -20,34 +18,40 @@ describe Resque::JobPerformer do
           :after_two
         ]
       }
-    end
+    }
 
     it 'runs the before hooks' do
-      @mock.expect :before_one, true, @job_args
-      @mock.expect :before_two, true, @job_args
-      @mock.expect :perform, true, @job_args
-      @mock.expect :after_one, true, @job_args
-      @mock.expect :after_two, true, @job_args
-      @job_performer.perform(@mock, @job_args, @options).must_equal true
-      @mock.verify
+      mock.expect :before_one, true, job_args
+      mock.expect :before_two, true, job_args
+      mock.expect :perform, true, job_args
+      mock.expect :after_one, true, job_args
+      mock.expect :after_two, true, job_args
+      job_performer.perform(mock, job_args, options).must_equal true
+      mock.verify
     end
 
-    it 'returns false when a before mock raises DontPerform' do
-      @options = {
-        :before => [:before_one],
-        :after  => [],
-        :around => []
+    describe "mock raises DontPerform" do
+      let(:options) {
+        {
+          :before => [:before_one],
+          :after  => [],
+          :around => []
+        }
       }
-      def @mock.before_one(*args)
-        raise Resque::DontPerform
+
+      it 'should return false' do
+        def mock.before_one(*args)
+          raise Resque::DontPerform
+        end
+
+        mock.expect :perform, nil, job_args
+        job_performer.perform(mock, job_args, options).must_equal false
       end
-      @mock.expect :perform, nil, @job_args
-      @job_performer.perform(@mock, @job_args, @options).must_equal false
     end
 
     describe 'when around_perform is present' do
-      before do
-        @options = {
+      let(:options) {
+        {
           :before => [],
           :around => [
             :around_one,
@@ -55,22 +59,22 @@ describe Resque::JobPerformer do
           ],
           :after => []
         }
-      end
+      }
 
       it 'runs the around hooks' do
-        @mock.expect :perform, true, @job_args
-        @mock.expect :around_two, true, @job_args
-        @mock.expect :around_one, true, @job_args
-        def @mock.around_two(*args)
+        mock.expect :perform, true, job_args
+        mock.expect :around_two, true, job_args
+        mock.expect :around_one, true, job_args
+        def mock.around_two(*args)
           method_missing(:around_two, *args)
           yield
         end
-        def @mock.around_one(*args)
+        def mock.around_one(*args)
           method_missing(:around_one, *args)
           yield
         end
-        @job_performer.perform(@mock, @job_args, @options)
-        @mock.verify
+        job_performer.perform(mock, job_args, options)
+        mock.verify
       end
     end
   end
