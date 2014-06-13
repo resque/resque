resque/resque

commit 10912efdcec91de7486e273f203c884db5f523f8

change default sort form asc to desc

lib/resque/failure/redis_multi_queue.rb
@@ -33,17 +33,20 @@ module Resque
         end
       end
 
-      def self.all(offset = 0, limit = 1, queue = :failed)
-        Resque.list_range(queue, offset, limit)
+      def self.all(offset = 0, limit = 1, queue = :failed, order = 'desc')
+        Resque.list_range(queue, offset, limit, order)
       end
 
       def self.queues
         Array(Resque.redis.smembers(:failed_queues))
       end
 
-      def self.each(offset = 0, limit = self.count, queue = :failed, class_name = nil)
-        items = all(offset, limit, queue)
+      def self.each(offset = 0, limit = self.count, queue = :failed, class_name = nil, order = 'desc')
+        items = all(offset, limit, queue, order)
         items = [items] unless items.is_a? Array
+        if order.eql? 'desc'
+          items.reverse!
+        end
         items.each_with_index do |item, i|
           if !class_name || (item['payload'] && item['payload']['class'] == class_name)
             yield offset + i, item

lib/resque/failure.rb
@@ -76,8 +76,8 @@ module Resque
     end
 
     # Iterate across all failures with the given options
-    def self.each(offset = 0, limit = self.count, queue = nil, class_name = nil, &block)
-      backend.each(offset, limit, queue, class_name, &block)
+    def self.each(offset = 0, limit = self.count, queue = nil, class_name = nil, order = 'desc', &block)
+      backend.each(offset, limit, queue, class_name, order, &block)
     end
 
     # The string url of the backend's web interface, if any.

lib/resque/failure/redis.rb
@@ -38,8 +38,12 @@ module Resque
         Resque.list_range(:failed, offset, limit)
       end
 
-      def self.each(offset = 0, limit = self.count, queue = :failed, class_name = nil)
-        Array(all(offset, limit, queue)).each_with_index do |item, i|
+      def self.each(offset = 0, limit = self.count, queue = :failed, class_name = nil, order = 'desc')
+        all_items = Array(all(offset, limit, queue))
+        if order.eql? 'desc'
+          all_items.reverse!
+        end
+        all_items.each_with_index do |item, i|
           if !class_name || (item['payload'] && item['payload']['class'] == class_name)
             yield offset + i, item
           end

lib/resque/server/helpers.rb
@@ -25,15 +25,19 @@ Resque::Server.helpers do
   end
 
   def failed_start_at
-    params[:start].to_i
+    (params[:start] || failed_size - failed_per_page).to_i
   end
 
   def failed_end_at
     if failed_start_at + failed_per_page > failed_size
       failed_size
     else
-      failed_start_at + failed_per_page
+      failed_start_at  + failed_per_page
     end
+  end
+
+  def failed_order
+    params[:order] || 'desc'
   end
 
   def failed_class_counts(queue = params[:queue])

lib/resque/server/views/failed.erb
@@ -18,8 +18,9 @@
 <% else %>
 <p class='sub'>Showing <%= failed_start_at %> to <%= failed_end_at %> of <b><%= failed_size %></b> jobs</p>
 
+
 <ul class='failed'>
-  <% Resque::Failure.each(failed_start_at, failed_per_page, params[:queue], params[:class]) do |id, job| %>
+  <% Resque::Failure.each(failed_start_at, failed_per_page, params[:queue], params[:class], failed_order) do |id, job| %>
     <%= partial :failed_job, :id => id, :job => job %>
   <% end %>
 </ul>

lib/resque/server/views/next_more.erb
@@ -3,20 +3,20 @@
 <% per_page ||= 20 %>
 <%if  start - per_page >= 0 || start + per_page <= size%>
 <p class='pagination'>
-  <% if start - per_page >= 0 %>
-    <a href="<%= current_page %>?start=<%= start - per_page %>" class='less'>&laquo; Previous</a>
+  <% if start + per_page <= size %>
+    <a href="<%= current_page %>?start=<%= start + per_page %>" class='more'>&laquo; Next</a>
   <% end %>
 
-  <% (0...(size / per_page.to_f).ceil).each do |page_num| %>
+  <% (size / per_page.to_f - 1).ceil.downto(0).each do |page_num| %>
     <% if start == page_num * per_page %>
-       <%= page_num + 1 %>
+       <%= page_num %>
     <% else %>
-      <a href="<%= current_page %>?start=<%= page_num * per_page %>"> <%= page_num + 1 %></a>
+      <a href="<%= current_page %>?start=<%= page_num * per_page %>"> <%= page_num %></a>
     <% end %>
   <% end %>
 
-  <% if start + per_page <= size %>
-    <a href="<%= current_page %>?start=<%= start + per_page %>" class='more'>Next &raquo;</a>
+  <% if start - per_page >= 0 %>
+    <a href="<%= current_page %>?start=<%= start - per_page %>" class='less'>Previous &raquo;</a>
   <% end %>
 </p>
 <%end%>

commit 04044ec3cfd47e226d17bc1b9cae11de785c82cd

Potential fix for #1230: failure multi-queue vs resque web
Fixes regression caused in [10912ef][], which caused an exception to be
raised in resque web when hitting /resque/failed or /resque/failed/:queue_name

[10912ef]: https://github.com/resque/resque/commit/10912efdcec91de7486e273f203c884db5f523f8

lib/resque/failure/redis_multi_queue.rb
@@ -33,8 +33,8 @@ module Resque
         end
       end
 
-      def self.all(offset = 0, limit = 1, queue = :failed, order = 'desc')
-        Resque.list_range(queue, offset, limit, order)
+      def self.all(offset = 0, limit = 1, queue = :failed)
+        Resque.list_range(queue, offset, limit)
       end
 
       def self.queues
@@ -42,7 +42,7 @@ module Resque
       end
 
       def self.each(offset = 0, limit = self.count, queue = :failed, class_name = nil, order = 'desc')
-        items = all(offset, limit, queue, order)
+        items = all(offset, limit, queue)
         items = [items] unless items.is_a? Array
         if order.eql? 'desc'
           items.reverse!