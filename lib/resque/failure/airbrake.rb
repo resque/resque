6f802b2da78db55b0e079b71e727d555d6f4bfae
Alejandro Martinez Ruiz <amr@redhat.com>
2018-02-01 16:05:59 +0000

    failure/airbrake: use the top-level ::Airbrake constant
    This was testing for ::Airbrake to respond to a specific method, so
    use ::Aibrake as well when calling the method.

--- lib/resque/failure/airbrake.rb
+++ lib/resque/failure/airbrake.rb
@@ -37,10 +37,10 @@ module Resque
 
       def notify(exception, options)
         if ::Airbrake.respond_to?(:notify_sync)
-          Airbrake.notify_sync(exception, options)
+          ::Airbrake.notify_sync(exception, options)
         else
           # Older versions of Airbrake (< 5)
-          Airbrake.notify(exception, options)
+          ::Airbrake.notify(exception, options)
         end
       end
     end
