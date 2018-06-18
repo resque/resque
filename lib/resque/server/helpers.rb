a6fa88ee646035f4d541db7f10979d55995838c7
Vitali Tsevan <v.tsevan@catawiki.nl>
2018-06-18 17:09:53 +0200

    Explicitely check RedisMultiQueue backend is enabled in failed_multiple_queues? helper

--- lib/resque/server/helpers.rb
+++ lib/resque/server/helpers.rb
@@ -9,7 +9,9 @@ Resque::Server.helpers do
 
   def failed_multiple_queues?
     return @multiple_failed_queues if defined?(@multiple_failed_queues)
-    @multiple_failed_queues = Resque::Failure.queues.size > 1
+
+    @multiple_failed_queues = Resque::Failure.queues.size > 1 ||
+      Resque::Failure.backend == Resque::Failure::RedisMultiQueue
   end
 
   def failed_size
