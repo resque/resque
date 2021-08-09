9c41f84d1ae6076eaa072a2bbc35ea9cfb5fab89
Jan Sandbrink <jan.sandbrink@aroundhome.de>
2021-08-09 11:47:19 +0200

    Fix delegation of remove from Failure::Multiple
    This method did not properly forward calls to its descendant
    backends.

--- lib/resque/failure/multiple.rb
+++ lib/resque/failure/multiple.rb
@@ -64,8 +64,8 @@ module Resque
         classes.first.requeue_queue(queue)
       end
 
-      def self.remove(index, queue)
-        classes.each { |klass| klass.remove(index) }
+      def self.remove(index, queue = nil)
+        classes.each { |klass| klass.remove(index, queue) }
       end
     end
   end
